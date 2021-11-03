a496acf7eb1954bd14a76500639b4ae0b3c95659
Pavels Saveljevs <p.saveljevs@gmail.com>
2021-11-03 20:05:42 +0200

    Updated prod env config

--- apps/cards/src/environments/environment.prod.ts
+++ apps/cards/src/environments/environment.prod.ts
@@ -2,11 +2,13 @@ import CONST from '../../../../definitions/constants.json';
 import * as RULES from '../../../../definitions/rules.json';
 import STRAVA_CONFIG from '../../../../definitions/stravaConfig.json';
 
+const baseUrl = 'http://104.248.43.19/';
+
 export const environment: any = {
     production: true,
     const: CONST,
     rules: RULES,
     stravaConfig: STRAVA_CONFIG,
-    authReturnUrl: 'http://localhost:4600/login/return',
-    baseBE: 'http://localhost:80/api',
+    authReturnUrl: `${baseUrl}/login/return`,
+    baseBE: `${baseUrl}:90/api`,
 };
