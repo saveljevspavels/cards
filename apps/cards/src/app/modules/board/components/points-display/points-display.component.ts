c3bf2a917e214bfc3a54705ad944133cea9c9a11
saveljevspavels <40292935+saveljevspavels@users.noreply.github.com>
2023-07-04 22:38:39 +0300

    Position in Points widget

--- apps/cards/src/app/modules/board/components/points-display/points-display.component.ts
+++ apps/cards/src/app/modules/board/components/points-display/points-display.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ConstService} from "../../../../services/const.service";
 import {ScoreService} from "../../../../services/score.service";
+import {AuthService} from "../../../../services/auth.service";
+import Score from "../../../../../../../shared/interfaces/score.interface";
 
 @Component({
   selector: 'app-points-display',
@@ -12,10 +14,31 @@ export class PointsDisplayComponent implements OnInit {
   public RULES = ConstService.RULES;
   public myScore$ = this.scoreService.myScore;
 
-  constructor(public scoreService: ScoreService) { }
+  public position: string;
+
+  constructor(
+      private scoreService: ScoreService,
+      private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.scoreService.scores.subscribe((scores) => {
+      this.position = this.numberToPosition(scores.findIndex((score: Score) => score.athleteId === this.authService.myId.value) + 1);
+    })
+  }
 
+  numberToPosition(number: number): string {
+    const suffixes = ['st', 'nd', 'rd'];
+    const exceptions = [11, 12, 13];
+    const mod = number % 10;
+
+    if (exceptions.includes(number)) {
+      return number + 'th';
+    } else if (mod <= 3) {
+      return number + suffixes[mod - 1];
+    } else {
+      return number + 'th';
+    }
   }
 
 }
