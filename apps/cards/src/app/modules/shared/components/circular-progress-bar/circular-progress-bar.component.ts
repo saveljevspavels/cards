f9a842871ec6c9f3216f484babe0cc6b88ece415
saveljevspavels <40292935+saveljevspavels@users.noreply.github.com>
2024-07-29 00:46:53 +0300

    Level view redesign 1st stage

--- apps/cards/src/app/modules/shared/components/circular-progress-bar/circular-progress-bar.component.ts
+++ apps/cards/src/app/modules/shared/components/circular-progress-bar/circular-progress-bar.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnChanges, OnInit, Output} from '@angular/core';
 import {ValidationService} from "../../../../services/validation.service";
 import {StaticValidationService} from "../../../../../../../shared/services/validation.service";
 import {RULES} from "../../../../../../../../definitions/rules";
+import {ChallengeStatType} from "../../../../../../../shared/interfaces/progressive-challenge.interface";
 
 @Component({
   selector: 'app-circular-progress-bar',
@@ -13,14 +14,16 @@ export class CircularProgressBarComponent implements OnChanges {
   @Input() type: string;
   @Input() activity: any;
   @Input() currentProgress: number;
+  @Input() currentValue: number;
   @Input() monochrome = false;
   @Input() small = false;
   @Input() reward = 0;
+  @Input() targetValue = 0;
+  @Input() challengeStat: ChallengeStatType;
 
   public active: boolean = true;
   public progress: number;
   public totalProgress: number;
-  public currentValue: number;
   public baseValue: number;
   public newValue: number;
   public totalValue: number;
@@ -36,8 +39,12 @@ export class CircularProgressBarComponent implements OnChanges {
 
   ngOnChanges(): void {
     this.activityType = StaticValidationService.normalizeActivityType(this.activity);
-    this.baseValue = this.validationService.getBaseValue(this.activity ? this.activityType : this.type);
-    this.currentValue = Math.floor((this.currentProgress * this.baseValue) / RULES.PROGRESS_PRECISION);
+    this.baseValue = this.targetValue || this.validationService.getBaseValue(this.activity ? this.activityType : this.type);
+    if(this.currentProgress) {
+      this.currentValue = Math.floor((this.currentProgress * this.baseValue) / RULES.PROGRESS_PRECISION);
+    } else if (this.currentValue) {
+        this.currentProgress = Math.floor((this.currentValue * RULES.PROGRESS_PRECISION) / this.baseValue);
+    }
     this.newValue = this.active ? parseInt(this.activity && this.activity[StaticValidationService.baseActivityTypeMap.get(this.activityType) || ''] || 0,  10) : 0;
     this.totalValue = this.active ? this.newValue + this.currentValue : this.currentValue;
     this.progress = this.activity ? this.validationService.getBaseCardProgress(this.activity) : 0;
@@ -66,10 +73,11 @@ export class CircularProgressBarComponent implements OnChanges {
           this.strokeColor = '#f06449';
           break;
         default:
-          this.strokeColor = '#fff';
+          this.strokeColor = '#494ADB';
 
       }
     }
   }
 
+  protected readonly ChallengeStatType = ChallengeStatType;
 }
